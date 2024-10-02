c166ba3dca24fd6542b77efb952cebf16b942062
deniaverkade <9024406@student.zadkine.nl>
2024-10-02 11:00:48 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+document.getElementById("header1").style.fontFamily= "Comic Sans MS";
+document.getElementById("header1").style.color="#0000FF";
+document.getElementById("header1").style.fontSize="75px";
